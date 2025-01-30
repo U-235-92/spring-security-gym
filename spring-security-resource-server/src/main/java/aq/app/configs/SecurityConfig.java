62fbc79249508d98f1c85a50235eb5214c601ed3
U-235-92 <a745296@yandex.ru>
2025-01-30 15:43:37 +0400

    Added example of using AuthenticationManagerResolver

--- spring-security-resource-server/src/main/java/aq/app/configs/SecurityConfig.java
+++ spring-security-resource-server/src/main/java/aq/app/configs/SecurityConfig.java
@@ -3,10 +3,20 @@ package aq.app.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 import aq.app.converters.JwtAuthenticationConverter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -30,12 +40,47 @@ public class SecurityConfig {
 //				.jwt(jwtCust -> jwtCust
 //					.jwkSetUri(keySetURI)
 //					.jwtAuthenticationConverter(converter)));
+//		http.oauth2ResourceServer(oauthCust -> oauthCust
+//				.opaqueToken(tokenCust -> tokenCust
+//						.introspectionUri(introspectionUri)
+//						.introspectionClientCredentials(resourceServerClientID, resourceServerSecret)));
 		http.oauth2ResourceServer(oauthCust -> oauthCust
-				.opaqueToken(tokenCust -> tokenCust
-						.introspectionUri(introspectionUri)
-						.introspectionClientCredentials(resourceServerClientID, resourceServerSecret)));
+				.authenticationManagerResolver(authenticationManagerResolver(jwtDecoder(), opaqueTokenIntrospector())));
 		http.authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated());
 		http.csrf(customizer -> customizer.disable());
 		return http.build();
 	}
+	
+	@Bean
+	AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver(JwtDecoder jwtDecoder, OpaqueTokenIntrospector opaqueTokenIntrospector) {
+		AuthenticationManager jwtAuth = new ProviderManager(new JwtAuthenticationProvider(jwtDecoder));
+		AuthenticationManager opaqueAuth = new ProviderManager(new OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector));
+		return request -> {
+			if("jwt".equals(request.getHeader("type")))
+				return jwtAuth;
+			else {
+				return opaqueAuth;
+			}
+		};
+	}
+	
+	@Bean
+	JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withJwkSetUri("http://localhost:5080/oauth2/jwks").build();
+	}
+
+	@Bean
+	OpaqueTokenIntrospector opaqueTokenIntrospector() {
+		return new SpringOpaqueTokenIntrospector("http://localhost:5090/oauth2/introspect", "client", "secret");
+	}
+	
+//	It uses when all the servers use the same token (like jwt or opaque)
+//	@Bean
+//	AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
+//		String authServerOne = "http://localhost:5082";
+//		String authServerTwo = "http://localhost:8052";
+//		JwtIssuerAuthenticationManagerResolver resolver = JwtIssuerAuthenticationManagerResolver
+//				.fromTrustedIssuers(authServerOne, authServerTwo);
+//		return resolver;
+//	}
 }
