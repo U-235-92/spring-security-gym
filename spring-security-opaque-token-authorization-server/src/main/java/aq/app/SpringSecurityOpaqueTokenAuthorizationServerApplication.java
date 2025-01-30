958d59d38f4ae73f0d52850a02634846991fd296
U-235-92 <a745296@yandex.ru>
2025-01-30 15:46:37 +0400

    Added opaque token auth server

---
+++ spring-security-opaque-token-authorization-server/src/main/java/aq/app/SpringSecurityOpaqueTokenAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package aq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOpaqueTokenAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOpaqueTokenAuthorizationServerApplication.class, args);
+	}
+
+}
