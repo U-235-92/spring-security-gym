6df99af3c9998c6604f3129d1116e7e7481cb252
U-235-92 <a745296@yandex.ru>
2025-01-30 16:07:34 +0400

    Added jwt token auth server

---
+++ spring-security-jwt-token-authorization-server/src/test/java/aq/app/SpringSecurityJwtTokenAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package aq.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtTokenAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
