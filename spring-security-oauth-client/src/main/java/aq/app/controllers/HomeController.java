d48466dfb933868c69282e5d22177d2b37982c70
U-235-92 <a745296@yandex.ru>
2025-01-31 15:22:19 +0400

    Added authentication log info

--- spring-security-oauth-client/src/main/java/aq/app/controllers/HomeController.java
+++ spring-security-oauth-client/src/main/java/aq/app/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package aq.app.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Authentication authentication) {
+		log.info(authentication.toString());
 		return "home.html";
 	}
 }
